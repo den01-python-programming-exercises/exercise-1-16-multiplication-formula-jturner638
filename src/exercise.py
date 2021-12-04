eb6f9c382f14945f3e82d0ffd0e883f93bd23937
Justin Turner <mrfathed@protonmail.com>
2021-12-04 10:18:34 -0600

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #write your code below this line
+    first = int(input("Give the first number:"))
+    second = int(input("Give the second number:"))
+
+    print("{} * {} = {}".format(first, second, first * second))
 
 if __name__ == '__main__':
     main()
